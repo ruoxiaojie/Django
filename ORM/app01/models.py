1d9a02187a14e76971e9a6e936574ca65af25a47
LJack <475030894@qq.com>
2017-08-29 18:37:55 +0800

    0829

---
+++ ORM/app01/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    nid=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=32)
+    price=models.DecimalField(max_digits=6,decimal_places=2)  #一共6位 xxxx.xx
+    publisher=models.ForeignKey("publish")
+    #外键建在一对多的 多的表中  建在子表
+    aushor=models.ManyToManyField("Author") #创建的第三张表
+
+class publish(models.Model):
+    name=models.CharField(max_length=32)
+    email=models.EmailField(max_length=32)
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name=models.CharField(max_length=32)
---
+++ ORM/app01/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render,HttpResponse
+from app01.models import *
+# Create your views here.
+
+def index(request):
+    return HttpResponse("okokok")
+
+def add(request):
+    # Book.objects.create(title="python",price=188) #一对一添加
+
+    # Book_obj=Book(title="python",price=188)
+    #Book_obj.save()
+    # #一对一添加
+
+
+    #一对多
+    # Book.objects.create(title="python",price=188,publisher_id=2) #添加方式1
+#一对多添加方式2
+    # Book_obj=Book.objects.get(nid=1)
+    # print(Book_obj.title)
+    # print(Book_obj.price)
+    # print(Book_obj.publisher) #publish object  需要在publish表添加__str__方法
+
+
+#多对多的添加方式
+    book_obj=Book.objects.get(nid=2)
+    # auth_list=Author.objects.all()
+    # book_obj.aushor.add(*auth_list) #nid=2的书籍作者的对象集合
+
+    book_obj.aushor.clear() #和id=2的书都删除
+    return HttpResponse("ok")
+'''
+  #  一对多的添加方式二
+    publish_obj=publish.objects.get(id=1)
+    book_obj=Book(title="linux",price=100)
+    book_obj.publisher=publish_obj
+    book_obj.save()   
+'''
+
+
+
+
+
