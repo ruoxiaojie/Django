e0153d99d70c6b853e794aba4bbb9ea54e95d281
LJack <475030894@qq.com>
2017-09-11 08:55:22 +0800

    xx

--- D/host_manager/my_mamager/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyMamagerConfig(AppConfig):
-    name = 'my_mamager'
--- D/host_manager/utils/myutils.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-
-class BaseReponse:
-    def __init__(self):
-        self.status = False
-        self.data = None
-        self.error = None
