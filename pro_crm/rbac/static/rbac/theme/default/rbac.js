0ec1debe559850fdad04c303b253eb411866cdbd
ruoxiaojie <475030894@qq.com>
2017-11-09 21:52:19 +0800

    xx

--- pro_crm/rbac/static/rbac/theme/default/rbac.js
+++
@@ -1,9 +0,0 @@
-$('.rbac-menu-header').click(function () {
-    var $icon = $(this).children().first();
-    if($icon.hasClass('glyphicon-folder-open')){
-        $icon.removeClass('glyphicon-folder-open').addClass('glyphicon-folder-close');
-    }else{
-        $icon.removeClass('glyphicon-folder-close').addClass('glyphicon-folder-open');
-    }
-    $(this).next().toggleClass('rbac-hide');
-});
